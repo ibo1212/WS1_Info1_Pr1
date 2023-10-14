8e3ed1706b6f01754fa3ce218194a71b42d059d8
ibrahimdrbas <ibrahim.abokheder@hotmail.com>
2023-10-14 11:38:32 +0200

    mistake return c ;

--- main.c
+++ main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, World!\n");
-    return 0;
-}
+
+
+
+    int main () {
+
+
+        int a = 1;
+        int b = 2, c= 3;
+
+      //  printf("Foo");
+
+        return c;
+    }
+
+
